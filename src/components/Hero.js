1e99e4dd7af1c99a0f540d02d2d2b9c1648f599f
Pab-lo <salaberry.pablo@gmail.com>
2022-07-24 17:18:12 +0200

    New sections

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <div className="hero--block">
+            <img src="../images/houses-grid.jpg" className="hero--photo"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
